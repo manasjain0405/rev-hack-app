760b658f85b0730c1e2670aa8eee8154de5766f5
AmulVerma <verma.amul7@gmail.com>
2020-01-18 11:58:18 +0530

    added Frontend

---
+++ app/src/main/java/com/example/manas/tnp/tpo/RecipeDetails.java
@@ -0,0 +1,23 @@
+package com.example.manas.tnp.tpo;
+
+public class RecipeDetails {
+
+    private String name;
+
+    public RecipeDetails() {
+    }
+
+    public RecipeDetails(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
