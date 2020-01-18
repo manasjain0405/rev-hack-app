ad65fbd8158d923927b7250ec0e165d2551558c2
AmulVerma <verma.amul7@gmail.com>
2020-01-18 15:42:33 +0530

    added field def

--- app/src/main/java/com/example/manas/tnp/tpo/EditDetailsActivity.java
+++ app/src/main/java/com/example/manas/tnp/tpo/EditDetailsActivity.java
@@ -8,7 +8,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,6 +18,9 @@ import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class EditDetailsActivity extends AppCompatActivity {
@@ -30,7 +35,8 @@ public class EditDetailsActivity extends AppCompatActivity {
     private EditText email;
     private EditText address;
     private Button submit;
-
+    private ListView steps;
+    private TextView ingredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,17 +49,23 @@ public class EditDetailsActivity extends AppCompatActivity {
         recipeName = findViewById(R.id.edit_description);
         submit = findViewById(R.id.submit);
 
-
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                steps=(ListView)findViewById(R.id.Steps);
                 RecipeDetails student_det = new RecipeDetails(recipeName.getText().toString());
                 Toast.makeText(EditDetailsActivity.this, "Searching recipie", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(EditDetailsActivity.this, MainActivity.class);
-                startActivity(intent);
+
+                //Intent intent = new Intent(EditDetailsActivity.this, MainActivity.class);
+                //startActivity(intent);
             }
         });
+        String ingre="Ingredients are :";
+        ingredients=(TextView)findViewById(R.id.ingredients);
+        ingredients.setText(ingre);
+        List<String> your_array_list = new ArrayList<String>();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,your_array_list);
+        steps.setAdapter(arrayAdapter);
     }
 }
 
